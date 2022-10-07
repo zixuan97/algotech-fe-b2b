afae5f592ca27298c238b3e7d263d2fc1c305a07
zixuan97 <ngzixuan1997@hotmail.com>
2022-10-07 09:35:52 +0800

    Update of apiRoot file dir and adding asyncFetchCallback

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,10 +1,5 @@
-export interface User {
-  id: number;
-  email: string;
-}
-
 export enum UserRole {
-  DISTRIBUTOR = 'DISTRIBUTOR', 
+  DISTRIBUTOR = 'DISTRIBUTOR',
   CORPORATE = 'CORPORATE'
 }
 
@@ -24,4 +19,58 @@ export interface User {
   role: UserRole;
   status: UserStatus;
   isVerified: boolean;
+}
+
+export interface SalesOrder {
+  id: number;
+  orderId: string;
+  customerName?: string;
+  customerAddress: string;
+  postalCode: string;
+  customerContactNo: string;
+  customerEmail?: string;
+  platformType: PlatformType;
+  createdTime: Date;
+  currency: string;
+  amount: number;
+  orderStatus: OrderStatus;
+  customerRemarks?: string;
+  salesOrderItems: SalesOrderItem[];
+}
+export interface SalesOrderItem {
+  id: number;
+  salesOrderId: number;
+  price: number;
+  quantity: number;
+  productName?: string;
+  createdTime?: Date;
+  isNewAdded?: boolean;
+  salesOrderBundleItems: SalesOrderBundleItem[];
+}
+
+export interface SalesOrderBundleItem {
+  id?: number;
+  salesOrderItemId?: number;
+  productName: string;
+  quantity: number;
+  isNewAdded?: boolean;
+}
+
+export enum PlatformType {
+  LAZADA = 'LAZADA',
+  REDMART = 'REDMART',
+  SHOPIFY = 'SHOPIFY',
+  SHOPEE = 'SHOPEE',
+  OTHERS = 'OTHERS'
+}
+
+export enum OrderStatus {
+  CREATED = 'CREATED',
+  PAID = 'PAID',
+  PREPARING = 'PREPARING',
+  PREPARED = 'PREPARED',
+  READY_FOR_DELIVERY = 'READY_FOR_DELIVERY',
+  SHIPPED = 'SHIPPED',
+  DELIVERED = 'DELIVERED',
+  CANCELLED = 'CANCELLED'
 }
