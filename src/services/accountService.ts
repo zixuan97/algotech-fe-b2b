3c4e1c2eb4639cc312f9285fa40489cb01f3790e
zixuan97 <ngzixuan1997@hotmail.com>
2022-10-11 05:32:42 +0800

    Append async to api calls

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -3,19 +3,19 @@ import { NewUserType } from '../components/account/RequestAccountModal';
 import { User } from '../models/types';
 import apiRoot from './util/apiRoot';
 
-export const getUserDetailsSvc = (userId: string): Promise<User> => {
+export const getUserDetailsSvc = async (userId: string): Promise<User> => {
   return axios.get(`${apiRoot}/user/details/${userId}`).then((res) => res.data);
 };
 
-export const editUserSvc = (user: User): Promise<any> => {
+export const editUserSvc = async (user: User): Promise<any> => {
   return axios.put(`${apiRoot}/user`, user).then((res) => res.data);
 };
 
-export const requestB2BUserSvc = (user: NewUserType): Promise<any> => {
+export const requestB2BUserSvc = async (user: NewUserType): Promise<any> => {
   return axios.post(`${apiRoot}/user`, user).then((res) => res.data);
 };
 
-export const updatePasswordSvc = (
+export const updatePasswordSvc = async (
   userEmail: string,
   currentPassword: string,
   newPassword: string
@@ -29,7 +29,7 @@ export const updatePasswordSvc = (
     .then((res) => res.data);
 };
 
-export const forgetPasswordSvc = (recipientEmail: string): Promise<any> => {
+export const forgetPasswordSvc = async (recipientEmail: string): Promise<any> => {
   return axios
     .post(`${apiRoot}/user/forgetpw`, { recipientEmail })
     .then((res) => res.data);
