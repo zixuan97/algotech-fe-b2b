9a42352d40d0f26b4b5723fd4d8841dea3abff36
zixuan97 <ngzixuan1997@hotmail.com>
2022-10-23 23:45:04 +0800

    Fix for autoclose after request for reqAcctModal

--- src/components/account/RequestAccountModal.tsx
+++ src/components/account/RequestAccountModal.tsx
@@ -34,7 +34,7 @@ const RequestAccountModal = ({ reqAccountModal, handleClose }: modalProps) => {
         });
         setLoading(false);
         form.resetFields();
-        setTimeout(() => handleClose, 3500);
+        setTimeout(() => handleClose(), 3500);
       },
       () => {
         setAlert({
