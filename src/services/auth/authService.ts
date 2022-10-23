a10e20e8661b306d8dae78299dee158eb5186ab2
zixuan97 <ngzixuan1997@hotmail.com>
2022-10-23 14:29:06 +0800

    Implement check for if user is disabled

--- src/services/auth/authService.ts
+++ src/services/auth/authService.ts
@@ -2,5 +2,11 @@
  * Used to make all API calls to authentication services
  */
 import axios from 'axios';
+import { User } from 'src/models/types';
+import apiRoot from '../util/apiRoot';
 
 const loginUser = (email: string, password: string) => {};
+
+export const getUserSvc = async (): Promise<User> => {
+  return axios.get(`${apiRoot}/user`).then((res) => res.data);
+};
